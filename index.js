c811af7389f706e4919c7d6b60716505c598cba2
Ikechukwu <iykekings36@gmail.com>
2019-07-28 19:18:13 +0100

    set / endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const server = require('./api/server');
 const PORT = process.env.PORT || 4000;
 
+server.get('/', (req, res) => {
+  res.status(200).json('Api exposed at /api');
+});
+
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
