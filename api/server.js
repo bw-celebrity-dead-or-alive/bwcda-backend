1570bc0bbce53f8007d42b81e813dfb1cf20364d
Ikechukwu <iykekings36@gmail.com>
2019-07-28 23:28:58 +0100

    scaffold celeb router

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const celebRouter = require('./celebrity/celeb.router');
 
 const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use('/api/celebrities', celebRouter);
 
 module.exports = server;
