93cf671a19e931ab2afbfc915a0625d05e36d125
Ikechukwu <iykekings36@gmail.com>
2019-08-01 21:08:47 +0100

    complete add test for players endpoint

--- test/players.test.js
+++ test/players.test.js
@@ -1,16 +1,22 @@
 const server = require('../api/server');
 const request = require('supertest');
 const app = request(server);
+const jwt = require('jsonwebtoken');
 
-// let id;
-// let celeb = {
-//   name: 'Stan White',
-//   info: 'Nigerian Developer',
-//   image_url:
-//     'http://static.tvmaze.com/uploads/images/medium_portrait/25/64971.jpg',
-//   birth: 1994,
-//   death: 0
-// };
+let id;
+let token;
+
+let player = {
+  name: 'Stan White',
+  email: 'stan@deal.com',
+  password: '12345'
+};
+
+beforeAll(async () => {
+  token = jwt.sign({ sub: 1, name: player.name }, process.env.JWT_SECRET, {
+    expiresIn: '1day'
+  });
+});
 
 describe('Players route', () => {
   it('[GET] /api/players', () => {
@@ -39,30 +45,70 @@ describe('Players route', () => {
         console.log(error);
       });
   });
-  // it('[GET] /api/celebrities/picture/:id', () => {
-  //   return app
-  //     .get('/api/celebrities/picture/1')
-  //     .expect(200)
-  //     .expect('Content-Type', /json/)
-  //     .then(res => {
-  //       expect(res.body).toEqual(expect.stringMatching(/^http/));
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // });
-  // it('[POST] /api/celebrities/ creates a new celebrity', () => {
-  //   return app
-  //     .post('/api/celebrities/picture/1')
-  //     .set('Accept', 'application/json')
-  //     .send(celeb)
-  //     .expect(201)
-  //     .expect('Content-Type', /json/)
-  //     .then(res => {
-  //       expect(res.body.name).toEqual(celeb.name);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // });
+  it('[GET] /api/players/:id/scores', () => {
+    return app
+      .get('/api/players/5/scores')
+      .set('Authorization', token)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(Object.keys(res.body[0])).toEqual(
+          expect.arrayContaining([
+            'score',
+            'player_id',
+            'email',
+            'name',
+            'id',
+            'created_at'
+          ])
+        );
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+  it('[POST] /api/auth/register adds a player to DB', () => {
+    return app
+      .post('/api/auth/register')
+      .set('Accept', 'application/json')
+      .send(player)
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        id = res.body.id;
+        expect(res.body.name).toEqual('Stan White');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+  it('[PUT] /api/players/:id update a player', () => {
+    return app
+      .put(`/api/players/${id}`)
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .send({ email: 'stan@dealing.com' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body.email).toBe('stan@dealing.com');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+  it('[DELETE] /api/players/:id removes a score from the leaderboard', () => {
+    return app
+      .delete(`/api/players/${id}`)
+      .set('Accept', 'application/json')
+      .set('Authorization', token)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body.name).toBe(player.name);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
 });
